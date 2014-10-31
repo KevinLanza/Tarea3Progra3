cac1d936e173f66621876dec6d430c01d8844677
KevinLanza <klanza03@gmail.com>
2014-10-31 13:09:45 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,70 +3,70 @@
 using namespace std;
 
 
-    class Animal
+    class Animal//Se crea una clase nueva
     {
     public:
-    string nombre;
-    int edad;
+    string nombre;//Se define una variable string para el nombre
+    int edad;//Se define una variable int para la edad
 
-    Animal()
+    Animal()//Se hace una funcion vacia de la clase
     {
     }
 
-    int getEdad()
+    int getEdad()//Funcion para obtener la edad
     {
 
-        return edad;
+        return edad;//Devuelve la edad 
     }
 
-    void setEdad(int edad)
+    void setEdad(int edad)//Funcion para asignar la edad
     {
-        this-> edad = edad;
+        this-> edad = edad;//La edad definida en la funcion es igual a la de la variable hecha 
     }
 
-    string getNombre()
+    string getNombre()//Funcion para obtener el nombre
     {
-        return nombre;
+        return nombre;//Devuelve el nombre
     }
 
-    void setNombre(string nombre)
+    void setNombre(string nombre)//Funcion para asignar el nombre
     {
-        this-> nombre = nombre;
+        this-> nombre = nombre;//El nombre definido en la funcion es igual a la de la variable hecha 
     }
 
 
     };
 
-    class Perro : public Animal
+    class Perro : public Animal//Se hace una clase perro que herede de la clse animal
     {
-        Perro(int edad, string nombre)
+        Perro(int edad, string nombre)//Se manda a llamar las funciones anteriores
         {
-            setEdad(edad);
-            setNombre(nombre);
+            setEdad(edad);//Se llama la funcion edad
+            setNombre(nombre);//Se llama la funcion nombre
         }
 
-        string ladrar()
+        string ladrar()//Se hace una funcion del perro para que ladre
         {
-            return "guau";
+            return "guau";//Devuelve la string "guau" 
         }
 
 
 
     };
 
-    class Gato : public Animal
+    class Gato : public Animal//Se hace una clase gato que herede de la clse animal
     {
         public:
 
-        Gato(int edad, string nombre)
+        Gato(int edad, string nombre)//Se manda a llamar las funciones anteriores
         {
-            setEdad(edad);
-            setNombre(nombre);
+            setEdad(edad);//Se llama la funcion edad
+            setNombre(nombre);//Se llama la funcion nombre
         }
 
-        string maullar()
+        string maullar()//Se hace una funcion del gato para que maulle 
         {
-            return "miau";
+            return "miau";//Devuelve la string "miau" 
         }
     };
 
